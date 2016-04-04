18ad68c398e8e3c701f1328380af383e08aee299
fwalraet <floris.walraet@district01.be>
2016-04-04 18:00:25 +0200

    DMOGSM-19 - Added configurable site theme

---
+++ og_sm_theme/README.md
@@ -0,0 +1,13 @@
+# Organic Groups : Theme
+This module allows to choose a theme per Site
+
+
+## Functionality
+* Site Administration menu when a Site context is active.
+* Hooks to register admin pages in the menu and in the overview pages.
+
+
+## Installation
+1. Enable the module.
+2. Open the admin settings (admin/config/administration/theme) and
+   choose one of the enabled themes for your Site.
---
+++ og_sm_theme/og_sm_theme.api.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * API documentation about the og_sm_theme module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Alter the list of allowed site themes for a Site.
+ *
+ * @param array $themes
+ *   The allowed site themes for this site
+ * @param array $context
+ *   The context - contains the site for which to alter the themes list.
+ */
+function hook_og_sm_theme_site_themes(&$themes, $context) {
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
