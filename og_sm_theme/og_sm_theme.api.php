ac65a3640f16c474cc4b188d853d80c4378e71d2
fwalraet <floris.walraet@district01.be>
2016-04-06 14:30:43 +0200

    DMOGSM-19 - Fixed coding standards, hook name and menu title

--- og_sm_theme/og_sm_theme.api.php
+++ og_sm_theme/og_sm_theme.api.php
@@ -13,18 +13,18 @@
  * Alter the list of allowed site themes for a Site.
  *
  * @param array $themes
- *   The allowed site themes for this Site
+ *   The allowed site themes for this Site.
  * @param array $context
  *   The context - contains the Site for which to alter the themes list.
  */
-function hook_og_sm_theme_site_themes(&$themes, $context) {
+function hook_og_sm_theme_site_themes_alter(&$themes, $context) {
 
 }
 
 /**
  * Alters theme operation links for a Site.
  *
- * @param $theme_groups
+ * @param array $theme_groups
  *   An associative array containing groups of themes.
  *
  * @see og_sm_theme_themes_page()
