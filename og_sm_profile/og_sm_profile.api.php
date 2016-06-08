db9075cf9a850801973ff51a703bc0d5bcb4c939
Geert van Dort <geert.van.dort@gmail.com>
2016-06-08 12:36:50 +0200

    DMOGSM-5: Alter the hook_og_sm_profile_view structure to use render callbacks

--- og_sm_profile/og_sm_profile.api.php
+++ og_sm_profile/og_sm_profile.api.php
@@ -16,20 +16,16 @@
  * file. The recommended place is in the yourmodule.og_sm.inc file as it keeps
  * your .module file cleaner and makes the platform load less code by default.
  *
- * @param object $profile
- *   The site user profile.
- *
  * @return array
- *   Array of sections, with the section machine name as key. Possible
+ *   Array of section ifo, with the section machine name as key. Possible
  *   attributes:
- *   - "label": (optional) The label of the section.
- *   - "elements": An array of renderable arrays to be displayed under the
- *   section.
- *   - "weight":
+ *   - "render callback": The function to call to display the section on the
+ *     profile page.
+ *   - "weight": (optional) The weight of the section.
  *
  * @see og_sm_profile_page()
  */
-function hook_og_sm_profile_view($profile) {
+function hook_og_sm_profile_view() {
   // @todo: Add example.
   return array();
 }
@@ -41,15 +37,13 @@ function hook_og_sm_profile_view($profile) {
  * file. The recommended place is in the yourmodule.og_sm.inc file as it keeps
  * your .module file cleaner and makes the platform load less code by default.
  *
- * @param array $output
+ * @param array $sections
  *   An associative array containing sections to be displayed on the profile
  *   page.
- * @param object $profile
- *   The site user profile.
  *
  * @see og_sm_profile_page()
  */
-function hook_og_sm_profile_view_alter(&$output, $profile) {
+function hook_og_sm_profile_view_alter(&$sections) {
   // @todo: Add example.
 }
 
