2d682ca3046d713a6e330aa31c210bd04b286651
Geert van Dort <geert.van.dort@gmail.com>
2016-12-22 16:04:52 +0100

    DMOGSM-111: Merge og_sm_feature_site_form_defaults hook in og_sm_feature_site_form hook

--- og_sm_feature/og_sm_feature.api.php
+++ og_sm_feature/og_sm_feature.api.php
@@ -69,14 +69,6 @@ function hook_og_sm_feature_form_defaults_alter(&$defaults, $feature) {
 
 }
 
-function hook_og_sm_feature_site_form_defaults($site, $feature) {
-
-}
-
-function hook_og_sm_feature_site_form_defaults_alter(&$defaults, $site, $feature) {
-
-}
-
 /**
  * @} End of "addtogroup hooks".
  */
