8ee650f42e95c2f1db7f58d426cbfe852226a296
Peter Decuyper <peter.decuyper@digipolis.gent>
2017-05-19 09:27:22 +0200

    PPL-1081: Use context instead of the $fieldset.

--- og_sm_comment/js/og-sm-comment-node-form.js
+++ og_sm_comment/js/og-sm-comment-node-form.js
@@ -10,10 +10,10 @@ Drupal.behaviors.commentFieldsetSummaries = {
     var $fieldset = $('fieldset.og-sm-comment-node-settings-form', context);
 
     // Set the summary based on the select value.
-    $fieldset.drupalSetSummary(function () {
-      var comment_default = $('input[name="comment_default"]', $fieldset).val();
-      var comment_override = $('input[@id="edit-comment-override"]:checked', $fieldset).length;
-      var comment_override_value = $('#edit-comment-override-value option:selected', $fieldset).text();
+    $fieldset.drupalSetSummary(function (context) {
+      var comment_default = $('input[name="comment_default"]', context).val();
+      var comment_override = $('input[@id="edit-comment-override"]:checked', context).length;
+      var comment_override_value = $('#edit-comment-override-value option:selected', context).text();
 
       if (!comment_override) {
         return Drupal.checkPlain(comment_default);
