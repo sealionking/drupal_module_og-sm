87139ca7ab610f142fb756fce2d38213dfe23ffa
Geert van Dort <geert.van.dort@gmail.com>
2017-07-14 11:54:52 +0200

    DMOGSM-117: Added config override logic for site specific logic

---
+++ og_sm_config/src/Config/SiteConfigFactoryOverrideInterface.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\og_sm_config\Config;
+
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Config\ConfigCollectionInfo;
+use Drupal\Core\Config\ConfigCrudEvent;
+use Drupal\Core\Config\ConfigFactoryOverrideBase;
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\ConfigRenameEvent;
+use Drupal\Core\Config\StorageInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+use Drupal\og_sm\SiteManagerInterface;
+
+/**
+ * Provides site overrides for the configuration factory.
+ */
+interface SiteConfigFactoryOverrideInterface extends ConfigFactoryOverrideInterface {
+
+  /**
+   * Get language override for given site and configuration name.
+   *
+   * @param \Drupal\node\NodeInterface $site
+   *   The site node.
+   * @param string $name
+   *   Configuration name.
+   *
+   * @return \Drupal\og_sm_config\Config\SiteConfigOverride
+   *   Configuration override object.
+   */
+  public function getOverride(NodeInterface $site, $name);
+
+  /**
+   * Returns the storage instance for a particular site.
+   *
+   * @param NodeInterface $site
+   *   The site node.
+   *
+   * @return \Drupal\Core\Config\StorageInterface
+   *   The storage instance for a particular site.
+   */
+  public function getStorage($site);
+
+  /**
+   * Gets the site node used to override configuration data.
+   *
+   * @return \Drupal\node\NodeInterface $site
+   *   The site node.
+   */
+  public function getSite();
+
+  /**
+   * Sets the site to be used in configuration overrides.
+   *
+   * @param \Drupal\node\NodeInterface $site
+   *   The site node.
+   *
+   * @return $this
+   */
+  public function setSite(NodeInterface $site);
+
+}
---
+++ og_sm_config/tests/modules/og_sm_config_test/src/Form/BasicForm.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\og_sm_config_test\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Path\AliasManagerInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Path\PathValidatorInterface;
+use Drupal\Core\Routing\RequestContext;
+use Drupal\og_sm_config\Form\SiteConfigFormBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configure site information settings for this site.
+ */
+class BasicForm extends SiteConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'og_sm.basic_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['system.site'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $site_config = $this->config('system.site');
+
+    $form['site_information'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Site details'),
+      '#open' => TRUE,
+    ];
+    $form['site_information']['site_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Site name'),
+      '#default_value' => $site_config->get('name'),
+      '#required' => TRUE,
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('system.site')
+      ->set('name', $form_state->getValue('site_name'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
