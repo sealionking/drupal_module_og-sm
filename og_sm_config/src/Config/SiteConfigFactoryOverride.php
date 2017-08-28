b8a2df3cee2d274259d7f151f61ac74aec7505e0
Geert van Dort <geert.van.dort@gmail.com>
2017-08-28 12:35:57 +0200

    Fixed method decleration issue

--- og_sm_config/src/Config/SiteConfigFactoryOverride.php
+++ og_sm_config/src/Config/SiteConfigFactoryOverride.php
@@ -123,7 +123,7 @@ class SiteConfigFactoryOverride extends ConfigFactoryOverrideBase implements Sit
   /**
    * {@inheritdoc}
    */
-  public function getStorage($site) {
+  public function getStorage(NodeInterface$site) {
     $collectionName = $this->createConfigCollectionName($site);
     if (!isset($this->storages[$collectionName])) {
       $this->storages[$collectionName] = $this->baseStorage->createCollection($collectionName);
